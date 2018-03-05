7de6dfa08b9cf73ec57ac1978fa0d8975f841988
chur7632 <chur7632@HRH-IC0027342.wrdsb.ca>
2018-03-05 10:23:09 -0500

    update

---
+++ src/Assignment4/A4Q6.java
@@ -0,0 +1,34 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Assignment4;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author chur7632
+ */
+public class A4Q6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // use a Scanner to read info
+        Scanner input = new Scanner (System.in);
+        
+        
+        // tell the user to ask for the speed limit 
+        System.out.println("Please enter the speed limit");
+        // make a String to store a name
+        String limit = input.nextLine();
+        // tell the user to ask for the speed at which the person was driving
+        System.out.println("Please enter the speed at which you were driving");
+        // make a String to store a name
+        String speed = input.nextLine();
+        
+        if 1 < speed < 20 = slowfine;
+    }
+}
